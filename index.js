cff39b2af4de58de6c28582fc80aea4fee805aec
SerhiiTrytenko <t3coder@gmail.com>
2021-12-16 15:10:52 +0200

    deploy

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 // connection express
-const express = require('express');
+const express = require("express");
 // connection mogoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+// connect dotenv
+require("dotenv").config(); // for for access process.env
+// import cors
+const cors = require("cors");
+
 // init app
 const app = express();
 // create a port on which it will run - since the process.env file is missing, we will use port 5000
 const PORT = process.env.PORT || 5000;
 
-// function for intermediate processing (middleware) -- for express read json (always top from middleware)
-app.use(express.json({extended: true}));
+// middleware -- for requests
+app.use(cors());
+
+// function for intermediate processing (middleware) -- for express read json
+app.use(express.json({ extended: true }));
 // function for intermediate processing (middleware) -- registration route
-app.use('/api/auth', require('./routes/auth.route'));
+app.use("/api/auth", require("./routes/auth.route"));
 // -- basket route
-app.use('/api/product', require('./routes/product.route'));
+app.use("/api/product", require("./routes/product.route"));
 
 // create a function in which we will connect to the database
 async function start() {
-    try {
-        await mongoose.connect('mongodb+srv://CoderFire:21212121@cluster0.glzcx.mongodb.net/womenUnderwear?retryWrites=true&w=majority', {
-          // object with setting > remove errors, warnings
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  try {
+    await mongoose.connect(
+      "mongodb+srv://CoderFire:21212121@cluster0.glzcx.mongodb.net/womenUnderwear?retryWrites=true&w=majority",
+      {
+        // object with setting > remove errors, warnings
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-        // call app
-        app.listen(PORT, () => {
-            console.log(`Server started on port ${PORT}`)
-        })
-    } catch (err) {
-        console.log(err);
-    }
+    // call app
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 start();
